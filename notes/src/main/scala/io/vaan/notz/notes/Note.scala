ea2cdbc9c08814fea4d8a521b50b61ffa700bb14
Vadim Anufriev <canny_wasp@vaan.io>
2020-07-25 23:40:44 +0300

    notes - note actor command handler

--- notes/src/main/scala/io/vaan/notz/notes/Note.scala
+++ notes/src/main/scala/io/vaan/notz/notes/Note.scala
@@ -1,8 +1,20 @@
 package io.vaan.notz.notes
 
+import io.vaan.notz.notes.NoteActor.NoteState
+
 final case class Note(
     userEmail: String,
     title: String,
     text: String,
     tags: Set[String]
 )
+
+object Note {
+  def apply(noteState: NoteState): Note =
+    Note(
+      userEmail = noteState.note.userEmail,
+      title = noteState.note.title,
+      text = noteState.note.text,
+      tags = noteState.note.tags
+    )
+}
--- notes/src/main/scala/io/vaan/notz/notes/NoteActor.scala
+++ notes/src/main/scala/io/vaan/notz/notes/NoteActor.scala
@@ -1,14 +1,21 @@
 package io.vaan.notz.notes
 
-import akka.actor.typed.Behavior
+import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.persistence.typed.PersistenceId
-import akka.persistence.typed.scaladsl.EventSourcedBehavior
+import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
 import io.vaan.notz.notes.utils.JsonSerializable
+import org.slf4j.{Logger, LoggerFactory}
 
 object NoteActor {
+  val log: Logger = LoggerFactory.getLogger(getClass)
+
   sealed trait Command  extends JsonSerializable
   sealed trait Event    extends JsonSerializable
+
+  final case class Create(note: Note, replyTo: ActorRef[CreateResponse]) extends Command
+  final case class Created(note: Note) extends Event
+  final case class CreateResponse(note: Note)
 
   final case class NoteState(note: Note)
   object NoteState {
@@ -28,8 +35,18 @@ object NoteActor {
     EventSourcedBehavior[Command, Event, NoteState](
       persistenceId = PersistenceId.ofUniqueId(note.title), // FIXME bad idea
       emptyState = NoteState.empty,
-      commandHandler = ???,
+      commandHandler = commandHandler,
       eventHandler = ???
     )
+  }
+
+  def commandHandler: (NoteState, Command) => Effect[Event, NoteState] = { (state, command) =>
+    command match {
+      case Create(note, replyTo) =>
+        log.info(s"Command received: Create($note)")
+        Effect
+          .persist(Created(note))
+          .thenReply(replyTo)(state => CreateResponse(Note(state)))
+    }
   }
 }
